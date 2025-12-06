72667f91f7a976a3c204adfb745b2ed54fdffa5e
CryptiqCO <167649856+CryptiqCO@users.noreply.github.com>
2025-12-06 10:06:00 -0600

    feat: scaffold mix project shell

---
+++ mix.exs
@@ -0,0 +1,21 @@
+﻿defmodule RelayPatch.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :relay_patch,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [extra_applications: [:logger]]
+  end
+
+  defp deps do
+    []
+  end
+end
